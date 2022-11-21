d67ceba4470272fc4df7ad6029576047031fabc1
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-11-21 23:29:16 +0300

    new

--- context/AnimationContext.js
+++ context/AnimationContext.js
@@ -19,109 +19,117 @@ const TransitionContext = createContext()
 export const AnimationContext = ({children}) => {
     const { ref:firstSection , inView: firstSecVis, entry } = useInView();
     const { ref:secondSection , inView: secondSecVis, entry:entrySecond } = useInView();
+    const { ref:thirdSection , inView: thirdSectionVis, entry:entryThird } = useInView();
+
 
     const cta1Ref = useRef()
     const cta2Ref = useRef()
     const cta3Ref = useRef()
+    const darkPageRef = useRef()
+    const buttonRef = useRef()
 
 
 
-    useLayoutEffect(() => {
+
+    // useEffect(() => {
        
 
-        if(firstSecVis) {
+    //     if(firstSecVis) {
         
-         const tlIntro = gsap.timeline({
-           scrollTrigger: {
-             trigger: entry.target,
-             start: '0%',
-             end: '40%',
+    //      const tlIntro = gsap.timeline({
+    //        scrollTrigger: {
+    //          trigger: entry.target,
+    //          start: '0%',
+    //          end: '100%',
+  
          
             
             
-             pin: true,
-             pinSpacing: false,
+    //          pin: true,
+    //          pinSpacing: false,
        
-           }
-         })
+    //        }
+    //      })
 
-         tlIntro.fromTo(cta1Ref.current, {opacity: 0, y: "100%"},{opacity: 1, y: "0%", duration:.6})
-         tlIntro.fromTo(cta2Ref.current, {opacity: 0, y: "100%"},{opacity: 1, y: "0%", duration:.6})
-         tlIntro.fromTo(cta3Ref.current, {opacity: 0, y: "100%"},{opacity: 1, y: "0%", duration:.6})
+    //     //  tlIntro.fromTo(cta1Ref.current, {opacity: 0, y: "100%"},{opacity: 1, y: "0%", duration:.4})
+    //     //  tlIntro.fromTo(cta2Ref.current, {opacity: 0, y: "100%"},{opacity: 1, y: "0%", duration:.4})
+    //     //  tlIntro.fromTo(cta3Ref.current, {opacity: 0, y: "100%"},{opacity: 1, y: "0%", duration:.4})
 
    
-        //  tlIntro.fromTo(mainVideoRef.current, { opacity:0}, {opacity: 1})
-        //  tlIntro.fromTo(firsTitleBox.current, {y:" -100%", opacity:0}, {opacity: 1, y:0, delay:.1})
-        //  tlIntro.fromTo(whiteArrow.current, {y:" -100%", opacity:0}, {opacity: 1, y:0, delay:.3})
-        //  tlIntro.fromTo(fairyRef.current, { y: "0%" }, {y: "-50%" , delay:1} )
+    //     //  tlIntro.fromTo(mainVideoRef.current, { opacity:0}, {opacity: 1})
+    //     //  tlIntro.fromTo(firsTitleBox.current, {y:" -100%", opacity:0}, {opacity: 1, y:0, delay:.1})
+    //     //  tlIntro.fromTo(whiteArrow.current, {y:" -100%", opacity:0}, {opacity: 1, y:0, delay:.3})
+    //     //  tlIntro.fromTo(fairyRef.current, { y: "0%" }, {y: "-50%" , delay:1} )
 
         
       
 
 
-        }
+    //     }
         
        
-         }, [firstSecVis]) 
+    //      }, [firstSecVis]) 
 
-         useLayoutEffect(() => {
-        
+
+         useEffect(() => {
              if(secondSecVis) {
-                 const tlSecond = gsap.timeline({
-                     scrollTrigger: {
-                         trigger: entrySecond.target,
-                    
-                         scrub: true,
-                         start: "-70%",
-                         end: "10%",
-                        
-                    
-
-                    }
-
-                    
-                 })
-                //  tlSecond.fromTo(secondText.current, {y: "-100%", opacity: 0}, {y: "y:0%", opacity:1,  stagger: 1, duration: 1})
-
-
-              
-               
-
-
-               
-                 
-
-
-         
-           
+                const tlIntro2 = gsap.timeline({
+                    scrollTrigger: {
+                      trigger: entrySecond.target,
+                      start: '-5%',
+                      end: '10%',
+                      scrub: true,
+                  
+                     
+                     
+                     
+                  
                 
+                    }
+                  })
+             tlIntro2.fromTo(cta1Ref.current, {opacity: 0, y: "100%"},{opacity: 1, y: "0%", duration:.4})
+             tlIntro2.fromTo(cta2Ref.current, {opacity: 0, y: "100%"},{opacity: 1, y: "0%", duration:.4}, "-5%")
+             tlIntro2.fromTo(cta3Ref.current, {opacity: 0, y: "100%"},{opacity: 1, y: "0%", duration:.4}, "-10%")
 
-          
+             }
+             
+             
 
- 
-          
+         },[secondSecVis])
+    
 
-        
-           
+         useEffect(() => {
+            if(secondSecVis) {
+               const tlIntro3 = gsap.timeline({
+                   scrollTrigger: {
+                     trigger: entrySecond.target,
+                     start: '-5%',
+                     end: '30%',
+                     scrub: true,
+                 
+                    
+                    
+                    
+                 
                
+                   }
+                 })
+            tlIntro3.fromTo(darkPageRef.current, { y: 0},{ y: -80 })
+            tlIntro3.fromTo(buttonRef.current, { y: 0},{ y: -80 })
+          
+           
 
             }
-
+            
             
 
-            
-          
-            
-             
+        },[secondSecVis])
 
-
-             
-         }, [secondSecVis])
-
+      
 
     return (
         <TransitionContext.Provider
-        value={{firstSection, secondSection, cta1Ref , cta2Ref, cta3Ref}}
+        value={{firstSection, secondSection,thirdSection,darkPageRef,buttonRef, cta1Ref , cta2Ref, cta3Ref}}
         >{children}</TransitionContext.Provider>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,16 @@ import useGsap from '../hooks/useGsap'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const {secondSection, firstSection, cta1Ref, cta2Ref, cta3Ref} = useGsap()
+  const {secondSection, firstSection,thirdSection, cta1Ref,buttonRef ,darkPageRef, cta2Ref, cta3Ref} = useGsap()
 
   return (
-   
-      <main className='main'>
-         <div className='lines-overlay'>
+    <>
+     <div className='lines-overlay'>
           <div className='line'></div>
           <div className='line'></div>
         </div>
-       
-        <div  ref={firstSection} className='main-video'>
+        <main className='main'>
+        <section  className='main-video'>
         <video  loop={true} muted={true} autoPlay={true} playsInline controls={false} className={styles.first__secVid}>
          <source
          src='/back.MP4'
@@ -25,27 +24,41 @@ export default function Home() {
          </source>
         </video>
         <div className='dark-overlay'></div>
-      </div>
+      </section>
+   
+        
+       
 
-        <section className='first-page'>
-          <h1></h1>
+        <section ref={secondSection} className='first-page'>
+          <div className='first-top'>
+          <h1>hello</h1>
+          </div>
+      
           <div className='first-text-box'>
               <h4 ref={cta1Ref}><span className="cta1">We help companies</span></h4>
               <h4 ref={cta2Ref}><span className="cta1">апыоврпалорыр </span></h4>
               <h4 ref={cta3Ref}><span className="cta1">ловращ</span></h4>
        
           </div>
-          {/* <div className='lines-overlay'>
-          <div className='line'></div>
-          <div className='line'></div>
-        </div> */}
+         <div className='first-page-btn'>
+         <div ref={buttonRef} className='first-contact-button'>
+            <h4>Contatc</h4>
+          </div>
+         </div>
+
+         <div ref={darkPageRef} className='firs-page-dark'>som</div>
+          
+
+       
     
         </section>
-        <section ref={secondSection} className='second-page'>
+        {/* <section  className='second-page'>
           third -pagr
-        </section>
+        </section> */}
       
-      </main>
+      </main></>
+   
+     
        
     
 
