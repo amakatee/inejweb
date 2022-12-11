a1a3cbefa1cc8ed50b312ac367ea5c06c41214bf
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-12-11 21:43:50 +0300

    slite changes

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react'
 import useGsap from '../hooks/useGsap'
 import gsap from 'gsap'
-
+import Link from 'next/link'
 const MobileNav = ({setNav, nav}) => {
   const {mobileNavRef} = useGsap()
 
@@ -25,11 +25,11 @@ const MobileNav = ({setNav, nav}) => {
 
   return (
     <div ref={mobileNavRef}  className='mobile-nav'>
-        <nav onClick={() => closeNav()}>close</nav>
+        <nav className='mob-nav' onClick={() => closeNav()}>закрыть</nav>
         <ul className='nav-list'>
-            <li>About</li>
-            <li>Price</li>
-            <li>Contact</li>
+            <Link href='/templates'><li>O Шаблонах</li></Link>
+            <Link href="/pricing"><li>Цены и сроки</li></Link>
+            <Link href='/workflow'><li>Сотрудничество</li></Link>
             
 
         </ul>
